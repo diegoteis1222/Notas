18e7ec666d7f4be39a7b2647e3979d2dd909555c
diegoteis122 <diegoteis122@gmail.com>
2025-07-01 01:17:12 +0200

    añadido numeros a las lineas de las notas

--- src/views/VentanaPrincipal.java
+++ src/views/VentanaPrincipal.java
@@ -1,20 +1,64 @@
 package views;
 
-import javax.swing.JFrame;
-import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
+import javax.swing.*;
+import javax.swing.event.*;
+import java.awt.*;
 
 public class VentanaPrincipal extends JFrame {
+    private JTextArea areaTexto;
+    private JTextArea lineas;
 
     public VentanaPrincipal() {
-
         setTitle("App de Notas");
-        setSize(400, 300);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(600, 400);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
 
-        JTextArea areaTexto = new JTextArea();
-        add(new JScrollPane(areaTexto));
+        areaTexto = new JTextArea();
+        areaTexto.setFont(new Font("Monospaced", Font.PLAIN, 14));
 
+        // Área para números de línea
+        lineas = new JTextArea("1");
+        lineas.setBackground(Color.LIGHT_GRAY);
+        lineas.setEditable(false);
+        lineas.setFont(new Font("Monospaced", Font.PLAIN, 14));
+
+        // Sincroniza scroll
+        JScrollPane scroll = new JScrollPane(areaTexto);
+        scroll.setRowHeaderView(lineas);
+
+        // Listener para actualizar líneas
+        areaTexto.getDocument().addDocumentListener(new DocumentListener() {
+            public String getLineas() {
+                int lineCount = areaTexto.getLineCount();
+                StringBuilder text = new StringBuilder();
+                for (int i = 1; i <= lineCount; i++) {
+                    text.append(i).append("\n");
+                }
+                return text.toString();
+            }
+
+            @Override
+            public void insertUpdate(DocumentEvent e) {
+                lineas.setText(getLineas());
+            }
+
+            @Override
+            public void removeUpdate(DocumentEvent e) {
+                lineas.setText(getLineas());
+            }
+
+            @Override
+            public void changedUpdate(DocumentEvent e) {
+                lineas.setText(getLineas());
+            }
+        });
+
+        add(scroll);
         setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new VentanaPrincipal();
     }
 }
